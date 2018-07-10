3f22cc58e9d4da8d43633ad08d4b7e056294dea8
adamewhite <evilofbanality@gmail.com>
2018-07-10 14:56:03 -0400

    heroku api

--- server.js
+++ server.js
@@ -47,13 +47,13 @@ app.use(function(req, res, next) {
 //   res.json({ message: 'API Initialized!'});
 // });
 
-// if (process.env.NODE_ENV === 'production') {
-// 	app.use(express.static('client/build'));
-// }
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
 
-// app.get('*',(req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// });
+app.get('*',(req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.use('/api', router);
 
