67e571f87c091440cd1741286a8fe65b9293975d
adamewhite <evilofbanality@gmail.com>
2018-07-11 12:52:49 -0400

    readjustment

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -95,8 +95,8 @@ class App extends Component {
                 <Route exact path={routes.SEARCH} render={() => (
                           this.state.authUser
                               ? <Search
-                                  trustsUrl="http://localhost:3100/api/trusts"
-                                  sitesUrl="http://localhost:3100/api/sites"
+                                  trustsUrl="https://bankruptcy.herokuapp.com/api/trusts"
+                                  sitesUrl="https://bankruptcy.herokuapp.com/api/sites"
                                 />
                               : <Redirect to={{
                                   pathname: '/'
