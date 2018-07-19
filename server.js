09d44a380da676770820ebb6b98fbd0622968ced
adamewhite <evilofbanality@gmail.com>
2018-07-19 12:36:59 -0400

    narrowed server

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get("/api/trusts", (req, res) => {
 });
 
 app.get("/api/sites", (req, res) => {
-      Site.find().limit(10).toArray(function(err, sites) {
+      Site.findOne().limit(10).toArray(function(err, sites) {
         if (err) {
           res.send(err);
         }
